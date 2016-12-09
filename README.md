dbe0cb69495202493e6c5e4dee5fddc5406c90b2
bartimar6@gmail.com <marek.bartik@ackee.cz>
2016-12-09 17:14:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Backup container for mongo instances to Google Storage
+
+This image provides a cron daemon that runs daily backups from mongo (clustered or single instance) to Google Storage.
+
+Following ENV variables must be specified:
+ - `MONGO_URL` contains the connection string for mongodump command line client (option -h)
+ - `MONGO_USER` contains the username
+ - `MONGO_PASSWORD` password of a user who has access to all dbs
+ - `GS_URL` contains address in GS where to store backups, without the `gs://` prefix
+  - `bucket-name/directory`
+ - `GS_ACCESS_KEY`
+ - `GS_SECRET_KEY`
+ - `CRON_SCHEDULE` cron schedule string, default '0 2 * * *'
+
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eo pipefail
+
+backup_tool="gsutil"
+
+# verify variables
+if [ -z "$GS_ACCESS_KEY" -o -z "$GS_SECRET_KEY" -o -z "$GS_URL" -o -z "$MONGO_URL" -o -z "$MONGO_USER" -o -z "$MONGO_PASSWORD" ]; then
+	echo >&2 'Backup information is not complete. You need to specify GS_ACCESS_KEY, GS_SECRET_KEY, GS_URL, MONGO_URL, MONGO_USER, MONGO_PASSWORD. No backups, no fun.'
+	exit 1
+fi
+
+# set gs config
+echo -e "[Credentials]\ngs_access_key_id = $GS_ACCESS_KEY\ngs_secret_access_key = $GS_SECRET_KEY" > /root/.boto
+
+# verify GS config
+$backup_tool ls "gs://$GS_URL" > /dev/null
+
+# set cron schedule TODO: check if the string is valid (five or six values separated by white space)
+[[ -z "$CRON_SCHEDULE" ]] && CRON_SCHEDULE='0 2 * * *' && \
+   echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
+
+# add a cron job
+echo "$CRON_SCHEDULE root rm -rf /tmp/dump && mongodump -h '$MONGO_URL' -u '$MONGO_USER' -p '$MONGO_PASSWORD' --out /tmp/dump/ --gzip >> /var/log/cron.log 2>&1 && $backup_tool mv /tmp/dump gs://$GS_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/dump" >> /etc/crontab
+crontab /etc/crontab
+
+exec "$@"
