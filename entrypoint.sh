1387cfcb957611728e27e8f85d34f041e425a93f
Marek Bartik <bartimar@users.noreply.github.com>
2016-12-23 16:58:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,15 +13,15 @@ fi
 # set gs config
 echo -e "[Credentials]\ngs_access_key_id = $GS_ACCESS_KEY\ngs_secret_access_key = $GS_SECRET_KEY" > /root/.boto
 
-# verify GS config
-$backup_tool ls "gs://$GS_URL" > /dev/null
+# verify gs config - ls bucket
+$backup_tool ls "gs://${GS_URL%%/*}" > /dev/null
 
 # set cron schedule TODO: check if the string is valid (five or six values separated by white space)
 [[ -z "$CRON_SCHEDULE" ]] && CRON_SCHEDULE='0 2 * * *' && \
    echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
 
 # add a cron job
-echo "$CRON_SCHEDULE root rm -rf /tmp/dump && mongodump -h '$MONGO_URL' -u '$MONGO_USER' -p '$MONGO_PASSWORD' --out /tmp/dump/ --gzip >> /var/log/cron.log 2>&1 && $backup_tool $backup_options /tmp/dump gs://$GS_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/dump" >> /etc/crontab
+echo "$CRON_SCHEDULE root mkdir -p /tmp/backup/ ; rm -rf /tmp/backup/* && mongodump -h '$MONGO_URL' -u '$MONGO_USER' -p '$MONGO_PASSWORD' --out /tmp/backup/dump.sql --gzip >> /var/log/cron.log 2>&1 && $backup_tool $backup_options /tmp/backup/ gs://$GS_URL/ >> /var/log/cron.log 2>&1" >> /etc/crontab
 crontab /etc/crontab
 
 exec "$@"
