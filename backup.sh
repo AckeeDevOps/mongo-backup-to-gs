d617cc15bcebffa3be03becc297e86ad2a765c91
Marek Bartik <bartimar@users.noreply.github.com>
2017-05-15 15:02:59 +0200

    fix cron->bash script single to double quotes

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ backup_options="-m rsync -r"
 
 mkdir -p /tmp/backup/
 rm -rf /tmp/backup/*
-mongodump -h '$MONGO_URL' -u '$MONGO_USER' -p '$MONGO_PASSWORD' --out /tmp/backup/
+mongodump -h "$MONGO_URL" -u "$MONGO_USER" -p "$MONGO_PASSWORD" --out /tmp/backup/
 find /tmp/backup -type f -exec gzip --fast {} \;
 find /tmp/backup -type f -size +4G -exec split -b 4G {} {}.part- \;
 find /tmp/backup -type f -name '*.gz' -size +4G -exec rm {} \;
