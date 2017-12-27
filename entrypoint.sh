aba18a8948e4b0809ff9ab3998bfe8d559a26bbe
Marek Bartik <bartimar@users.noreply.github.com>
2017-12-27 10:34:12 +0100

    no credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ backup_tool="/google-cloud-sdk/bin/gsutil"
 backup_options="-m rsync -r"
 
 # verify variables
-if [ -z "$GS_URL" -o -z "$MONGO_URL" -o -z "$MONGO_USER" -o -z "$MONGO_PASSWORD" ]; then
-	echo >&2 'Backup information is not complete. You need to specify GS_URL, MONGO_URL, MONGO_USER, MONGO_PASSWORD. No backups, no fun.'
+if [ -z "$GS_URL" -o -z "$MONGO_URL" ]; then
+	echo >&2 'Backup information is not complete. You need to specify GS_URL, MONGO_URL. No backups, no fun.'
 	exit 1
 fi
 
@@ -17,6 +17,6 @@ $backup_tool ls "gs://${GS_URL%%/*}" > /dev/null
 [[ -z "$CRON_SCHEDULE" ]] && CRON_SCHEDULE='0 2 * * *' && \
    echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
 
-echo "$CRON_SCHEDULE root mkdir -p /tmp/backup/ ; rm -rf /tmp/backup/* && mongodump -h '$MONGO_URL' -u '$MONGO_USER' -p '$MONGO_PASSWORD' --out /tmp/backup/ >> /var/log/cron.log 2>&1 && find /tmp/backup -type f ! -name '*.gz' -exec gzip --fast {} >> /var/log/cron.log 2>&1 \;  && find /tmp/backup -type f -size +4G -exec split -b 4G {} {}.part- >> /var/log/cron.log 2>&1 \;  && find /tmp/backup -type f -name '*.gz' -size +4G -exec rm {} >> /var/log/cron.log 2>&1 \;  && $backup_tool $backup_options /tmp/backup/ gs://$GS_URL/ >> /var/log/cron.log 2>&1" >> /etc/crontab
+echo "$CRON_SCHEDULE root mkdir -p /tmp/backup/ ; rm -rf /tmp/backup/* && mongodump -h '$MONGO_URL' --out /tmp/backup/ >> /var/log/cron.log 2>&1 && find /tmp/backup -type f ! -name '*.gz' -exec gzip --fast {} >> /var/log/cron.log 2>&1 \;  && find /tmp/backup -type f -size +4G -exec split -b 4G {} {}.part- >> /var/log/cron.log 2>&1 \;  && find /tmp/backup -type f -name '*.gz' -size +4G -exec rm {} >> /var/log/cron.log 2>&1 \;  && $backup_tool $backup_options /tmp/backup/ gs://$GS_URL/ >> /var/log/cron.log 2>&1" >> /etc/crontab
 
 exec "$@"
