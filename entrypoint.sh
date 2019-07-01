e193c42d4e9403378a439c3a799bebe6ad181eba
Tomáš Hejátko <tomas.hejatko@gmail.com>
2019-07-01 12:05:28 +0200

    Update entrypoint.sh
    Add --oplog dump option, so replica set backups are consistent

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,11 +28,16 @@ if [ ! -z "$MONGO_PORT" ]; then
   MONGO_PORT_CON=":${MONGO_PORT}"
 fi
 
+MONGO_DUMP_OPLOG=""
+if [ ! -z "$MONGO_DUMP_OPLOG" ]; then
+  MONGO_DUMP_OPLOG="--oplog"
+fi
+
 # verify gs config - ls bucket
 $backup_tool ls "gs://${GS_URL%%/*}" > /dev/null
 echo "Google storage bucket access verified."
 
 mkdir -p /tmp/backup/
 rm -rf -- /tmp/backup/* 
-mongodump -h "$MONGO_URL" -u "$MONGO_USER" -p "$MONGO_PASSWORD" --out /tmp/backup/dump --gzip 
+mongodump -h "$MONGO_URL" -u "$MONGO_USER" -p "$MONGO_PASSWORD" --out /tmp/backup/dump --gzip $MONGO_DUMP_OPLOG
 $backup_tool $backup_options /tmp/backup/ gs://$GS_URL/ 
